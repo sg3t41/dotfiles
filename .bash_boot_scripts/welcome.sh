bd064b0099a76ecfb263eb4a6132604b30000d9a
sg3t41 <sg3t41@gmail.com>
2024-10-14 16:32:42 +0900

    dev

--- .bash_boot_scripts/welcome.sh
+++ .bash_boot_scripts/welcome.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+welcome_message="welcome"
+
 # figletが存在しない場合は標準で出力
 if command -v figlet > /dev/null; then
-    output=$(figlet ready)
+    output=$(figlet $welcome_message)
 else
-    output="welcome"
+    output=$welcome_message
 fi
 
 echo "$output"
